56046aa0870ca5bc8a376b2d538e211d60346308
ktrmzm <k.maezmac@gmail.com>
2024-04-10 18:54:06 +0900

    メソッドなくす

--- index.js
+++ index.js
@@ -17,36 +17,8 @@ const postArticle = async () => {
     var title = "Amazonタイムセール " + year + "/" + month + "/" + day + " " + hour + ":" + minutes + "更新";
 
     console.log(title);
-    var contents = getFromAmazon();
-    console.log(contents);
 
-    const escaped = he.escape(contents);
-    const xmlData = `<?xml version="1.0" encoding="utf-8"?>
-    <entry xmlns="http://www.w3.org/2005/Atom">
-      <title>${title}</title>
-      <content>${escaped}</content>
-      <updated>${today.toISOString()}</updated>
-    </entry>`;
-
-    await axios.post(url, xmlData, {
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-        auth: {
-            username: process.env.HATENA_USERNAME,
-            password: process.env.HATENA_PASSWORD,
-        },
-    }).then((response) => {
-        if (response.status !== 201) {
-            console.log(response);
-        }
-    }).catch((error) => {
-        console.log(error)
-    });
-}
-
-const getFromAmazon = async () => {
-    var contentsText = "";
+    var contents = "";
     console.log("あ");
     https.get(process.env.AMAZON_API_URL, (resp) => {
         let data = '';
@@ -76,7 +48,7 @@ const getFromAmazon = async () => {
                     console.log(productPrice);
                     console.log(productDiscount);
 
-                    contentsText += `
+                    contents += `
                 <div class="hatena-asin-detail"><a href="${productUrl}" class="hatena-asin-detail-image-link" target="_blank" rel="noopener"><img src="${productImage}" class="hatena-asin-detail-image" alt="${productTitle}" title="${productTitle}" /></a>
                 <div class="hatena-asin-detail-info">
                 <p class="hatena-asin-detail-title"><a href="${productUrl}" target="_blank" rel="noopener">${productTitle}</a></p>
@@ -95,10 +67,34 @@ const getFromAmazon = async () => {
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
-    if (contentsText == "") {
-        getFromAmazon();
-    }
-    return contentsText;
+    // if (contents == "") {
+    // }
+
+    console.log(contents);
+
+    const escaped = he.escape(contents);
+    const xmlData = `<?xml version="1.0" encoding="utf-8"?>
+    <entry xmlns="http://www.w3.org/2005/Atom">
+      <title>${title}</title>
+      <content>${escaped}</content>
+      <updated>${today.toISOString()}</updated>
+    </entry>`;
+
+    await axios.post(url, xmlData, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+        auth: {
+            username: process.env.HATENA_USERNAME,
+            password: process.env.HATENA_PASSWORD,
+        },
+    }).then((response) => {
+        if (response.status !== 201) {
+            console.log(response);
+        }
+    }).catch((error) => {
+        console.log(error)
+    });
 }
 
 
