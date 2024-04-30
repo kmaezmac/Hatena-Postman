7ef694be86e3c88b83de0202d075670009bf5743
ktrmzm <k.maezmac@gmail.com>
2024-04-30 17:56:27 +0900

    pageをランダムに

--- index.js
+++ index.js
@@ -172,8 +172,9 @@ const getFromRakuten = async () => {
         process.env.AD_HTML002,
         process.env.AD_HTML003
     ];
+    var random = Math.floor(Math.random() * 34) + 1;
     var rakutenRankingUrl = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601?applicationId="
-        + process.env.RAKUTEN_APP_ID + "&sex=1&carrier=0&page=1&affiliateId=" + process.env.RAKUTEN_AFFILIATE_ID;
+        + process.env.RAKUTEN_APP_ID + "&sex=1&carrier=0&page=" + random + "&affiliateId=" + process.env.RAKUTEN_AFFILIATE_ID;
 
     try {
         const response = await axios.get(rakutenRankingUrl);
